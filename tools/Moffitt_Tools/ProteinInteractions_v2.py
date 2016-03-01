ff61595630f38868fd058579f0d815881233150a
bornea <bornea27@gmail.com>
2016-03-01 14:24:09 -0500

    Fix on interactions.

--- tools/Moffitt_Tools/ProteinInteractions_v2.py
+++ tools/Moffitt_Tools/ProteinInteractions_v2.py
@@ -12,11 +12,6 @@
 # with no Invariant Sections, no Front-Cover Texts, and no Back-Cover Texts.
 # A copy of the license is included in the section entitled "GNU
 # Free Documentation License".
-#######################################################################################
-import urllib2
-import itertools
-import sys
-import os
 ################################################################################
 ## REQUIRED INPUT ##
 
@@ -28,14 +23,26 @@ import os
 #       - high: >0.7
 #       - very high: >0.9
 # 4) Species: Human, Yeast, or Mouse
-###############################################################################
+################################################################################
+
+
+import urllib2
+import itertools
+import sys
+import os
+
+
 listfile = sys.argv[1]
 SAINT_cutoff = sys.argv[2]
 Int_conf = sys.argv[3]
 Species = sys.argv[4]
 cyto_file = sys.argv[5]
-db_path = r"/galaxy-apostl-docker/tools/Moffitt_Tools/"
-###############################################################################
+db_path = sys.argv[6]
+
+tool_path = r"/galaxy-apostl-docker/tools/Moffitt_Tools/"
+fasta_db = str(tool_path)  + "/SwissProt_HUMAN_2014_08.fasta"
+
+
 class ReturnValue1(object):
     def __init__(self, uniprot_acc, gene, swissprot):
         self.up = uniprot_acc
@@ -46,48 +53,42 @@ class ReturnValue2(object):
         self.data = getdata
         self.proteins = getproteins
         self.header = getheader
+
+
 def main(listfile, SAINT_cutoff, Int_conf, Species):
     cytoscape(dd_network(listfile, SAINT_cutoff, Int_conf), listfile, SAINT_cutoff)
+
+
 def readtab(infile):
-    with open(infile,'r') as x: # read in tab-delim text
+    with open(infile, 'r') as file_to_read:
+    # Read in tab-delim text.
         output = []
-        for line in x:
+        for line in file_to_read:
             line = line.strip()
             temp = line.split('\t')
             output.append(temp)
     return output
-def read_listfile(listfile): # Get data, proteins and header from scaffold output
+
+
+def read_listfile(listfile): 
+    # Get data, proteins and header from scaffold output
     dupes = readtab(listfile)
     header = dupes[0]
     prot_start = header.index("PreyGene")-1
-    data = dupes[1:] # cut off blank line and END OF FILE
+    data = dupes[1:]
+    # Cut off blank line and END OF FILE.
     proteins = []
     for protein in data:
         proteins.append(protein[prot_start])
     return ReturnValue2(data, proteins, header)
 
-def get_info(uniprot_accession_in): #get aa lengths and gene name
+
+def get_info(uniprot_accession_in): 
+    # Get aa lengths and gene name.
     error = open('error proteins.txt', 'a+')
-    i=0
-    while i==0:
-        try:
-            data = urllib2.urlopen("http://www.uniprot.org/uniprot/" + uniprot_accession_in + ".fasta")
-            break
-        except urllib2.HTTPError, err: 
-            i = i + 1
-            if i == 50:
-                sys.exit("More than 50 errors. Check your file or try again later.")
-            if err.code == 404:
-                error.write(uniprot_accession_in + '\t' + "Invalid URL. Check protein" + '\n')
-                seqlength = 'NA'
-                genename = 'NA'
-                return ReturnValue1(seqlength, genename)
-            elif err.code == 302:
-                sys.exit("Request timed out. Check connection and try again.")
-            else:
-                sys.exit("Uniprot had some other error")
+    data = open(fasta_db, 'r')
     lines = data.readlines()
-    header = lines[0] 
+    header = lines[0]
     lst = header.split('|')
     lst2 = lst[2].split(' ')
     swissprot = lst2[0]
@@ -100,7 +101,7 @@ def get_info(uniprot_accession_in): #get aa lengths and gene name
         return ReturnValue1(uniprot_acc, genename, swissprot)
     if lines != []:
         seqlength = 0
-        header = lines[0] 
+        header = lines[0]
         if 'GN=' in header:
             lst = header.split('GN=')
             lst2 = lst[1].split(' ')
@@ -112,33 +113,44 @@ def get_info(uniprot_accession_in): #get aa lengths and gene name
             error.close
             return ReturnValue1(uniprot_acc, genename, swissprot)
 
-def dd_network(listfile, SAINTscore, CPDB_filter): ## Filter by SS and CPDB
-    data = read_listfile(listfile).data # change to filtered list
+
+def dd_network(listfile, SAINTscore, CPDB_filter): 
+    # Filter by SS and CPDB.
+    data = read_listfile(listfile).data
+    # Change to filtered list.
     SS = (read_listfile(listfile).header).index("SaintScore")
     filt_data = []
-    for i in data: 
+    for i in data:
         if i[SS] >= SAINTscore:
             filt_data.append(i)
     accessions = []
     for i in filt_data:
         accessions.append(get_info(i[1]).sp)
-    GO=[]
+    GO = []
     for i in CPDB[2:]:
-        if i[3] >= CPDB_filter: # filter interaction confidence
-            GO.append(i[2]) # all known interactions
+        if i[3] >= CPDB_filter:
+        # Filter interaction confidence.
+            GO.append(i[2])
+            # All known interactions.
     GO2 = []
     for i in GO:
-        GO2.append(i.split(',')) # make interactions list friendly
+        GO2.append(i.split(','))
+        # Make interactions list friendly.
     unfiltered_network = {}
     for i in accessions:
         interactions = []
         for j in GO2:
-            if i in j: # find the interactions
-                if j not in interactions:# dont add duplicate interactions
+            if i in j:
+            # Find the interactions.
+                if j not in interactions:
+                # Dont add duplicate interactions.
                     interactions.append(j)
-        merged = list(itertools.chain(*interactions)) # flatten list of lists
-        unfiltered_network[i]=merged # assign all possible interactions to protein in a dictionary
-    dd_network = {} #data dependent network
+        merged = list(itertools.chain(*interactions))
+        # Flatten list of lists.
+        unfiltered_network[i] = merged
+        # Assign all possible interactions to protein in a dictionary.
+    dd_network = {}
+    # Data dependent network.
     for i in unfiltered_network:
         temp = []
         for j in unfiltered_network[i]:
@@ -146,16 +158,18 @@ def dd_network(listfile, SAINTscore, CPDB_filter): ## Filter by SS and CPDB
                 if j not in temp:
                     if j != i:
                         temp.append(j)
-        dd_network[i]=temp
+        dd_network[i] = temp
     return dd_network
+
+
 def cytoscape(dd_network, listfile, SAINTscore):
-    with open('network.sif','wt') as y:
+    with open('network.sif', 'wt') as y:
         data = read_listfile(listfile).data
         SS = (read_listfile(listfile).header).index("SaintScore")
         filt_data = []
-        for i in data: 
+        for i in data:
             if i[SS] >= SAINTscore:
-                filt_data.append(i)
+                filt_data.append(get_info(i[1]).sp)
         header = ["Prey", "Interactions"]
         header = '\t'.join(header)
         y.write(header + '\n')
@@ -166,12 +180,14 @@ def cytoscape(dd_network, listfile, SAINTscore):
                     lst.append(j)
                 for j in lst:
                     y.write(i[1]+'\t'+'pp'+'\t' + j+'\n')
+
+
 if Species == "Human":
-        CPDB = readtab(str(db_path) + 'ConsensusPathDB_human_PPI.txt')
+    CPDB = readtab(str(db_path) + 'ConsensusPathDB_human_PPI.txt')
 if Species == "Yeast":
-        CPDB = readtab(str(db_path) + 'ConsensusPathDB_yeast_PPI.txt')
+    CPDB = readtab(str(db_path) + 'ConsensusPathDB_yeast_PPI.txt')
 if Species == "Mouse":
-        CPDB = readtab(str(db_path) +'ConsensusPathDB_mouse_PPI.txt')
+    CPDB = readtab(str(db_path) +'ConsensusPathDB_mouse_PPI.txt')
 if __name__ == '__main__':
     main(listfile, SAINT_cutoff, Int_conf, Species)
     os.rename('network.sif', str(cyto_file))
